0e98d2c771541bddc52c57a195df9d4a11e28af5
mitchell <mitchell.bermin01@correo.usa.edu.co>
2023-09-01 19:50:08 -0500

    Se agrega un boton para dispara el mapa

--- map.js
+++ map.js
@@ -1,25 +1,48 @@
-// Crea un mapa en el div con id "map"
-var map = L.map('map').setView([0, 0], 2); // Centrado en latitud 0, longitud 0, y zoom 2
+// Referencias a los elementos HTML
+var mostrarMapaButton = document.getElementById('mostrarMapa');
+var mapContainer = document.getElementById('map-container');
 
-// Agrega una capa de mapa base
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// Función para mostrar el mapa
+function mostrarMapa() {
+  mapContainer.style.display = 'block'; // Muestra el contenedor del mapa
 
-// Obtiene la ubicación del usuario y agrega un marcador en el mapa
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var lat = position.coords.latitude;
-    var lng = position.coords.longitude;
+  // Crea un mapa en el div con id "map"
+  var map = L.map('map').setView([0, 0], 2); // Centrado en latitud 0, longitud 0, y zoom 2
 
-    var userMarker = L.marker([lat, lng]).addTo(map);
-    userMarker.bindPopup("Tu ubicación").openPopup();
+  // Agrega una capa de mapa base
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
 
-    map.setView([lat, lng], 15); // Cambia la vista del mapa a la ubicación del usuario
-  }, function() {
-    // Manejo de errores
-    alert('No se pudo obtener la ubicación');
-  });
-} else {
-  alert('La geolocalización no es compatible con este navegador.');
+  // Obtiene la ubicación del usuario y agrega un marcador en el mapa
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+
+      // Utiliza el servicio de geocodificación de Nominatim para obtener detalles del lugar
+      var apiUrl = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`;
+
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+          var locationInfo = data.display_name; // Obtiene la información del lugar
+          var userMarker = L.marker([lat, lng]).addTo(map);
+          userMarker.bindPopup(locationInfo).openPopup();
+        })
+        .catch(error => {
+          console.error('Error en la geocodificación:', error);
+        });
+
+      map.setView([lat, lng], 15); // Cambia la vista del mapa a la ubicación del usuario
+    }, function() {
+      // Manejo de errores
+      alert('No se pudo obtener la ubicación');
+    });
+  } else {
+    alert('La geolocalización no es compatible con este navegador.');
+  }
 }
+
+// Escucha el clic del botón para mostrar el mapa
+mostrarMapaButton.addEventListener('click', mostrarMapa);
