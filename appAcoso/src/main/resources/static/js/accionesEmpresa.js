a6bdb59fe5f6011012f8d49e3e69170afe4373ae
ChiaraUsa <chiara.valenzuela01@correo.usa.edu.co>
2023-09-12 10:43:12 -0500

    Creación Interfaz empresa, mod en centro de ayuda

---
+++ appAcoso/src/main/resources/static/js/accionesEmpresa.js
@@ -0,0 +1,16 @@
+
+function editarPerfil() {
+    alert("Editar perfil");
+ }
+ 
+ function irInicio(){
+    window.location.replace("inicioEmpresa.html");
+ }
+
+ function irInicio(){
+    window.location.replace("inicioEmpresa.html");
+ }
+
+ function irCentroAyuda(){
+    window.location.replace("CentroAyuda.html");
+ }
