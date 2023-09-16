88c186f812df9695d4b27ce8395dc686690c705c
GodCarlosRojas <109989522+GodCarlosRojas@users.noreply.github.com>
2023-09-16 14:12:16 -0500

    Se integro interfacez
    Menu, inicio, perfil y eventos, tambien de usuario en mapa y centro de ayuda

---
+++ static/js/login.js
@@ -0,0 +1,107 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+   //on ready
+});
+
+function iniciarSesion(){
+  let datos = {};
+
+  datos.email = document.querySelector('#txtEmail').value
+  datos.password = document.querySelector('#txtContraseña').value
+
+  rol = document.querySelector('#txtRol').value
+
+  if(rol == 'usuario')
+  {
+    inicioUsuario('usuario',datos)
+  }
+  else if(rol ==  'empresa')
+  {
+    inicioEmpresa('empresa', datos)
+  }
+  else if(rol ==  'administrador')
+  {
+    inicioAdmin('administrador', datos)
+  }
+  else
+  {
+    alert('Debe escoger un Rol!')
+    return;
+  }
+
+}
+
+function inicioUsuario(via, datos){
+  $.ajax({
+
+  		 url:"/api/auth/authenticate/"+via,
+  		 type:"POST",
+  		 contentType:"application/json",
+  		 dataType:"json",
+
+  		 data:JSON.stringify(datos),
+
+  		 success: function(rta) {
+  		     localStorage.email = datos.email
+  			 Cookies.set('token',rta['token']);
+  			 window.location.replace("inicio.html");
+  		 },
+  		 error: function(xhr, status) {
+  			 alert('Usuario no existente');
+  		 },
+  		 complete: function(xhr, status) {
+  			 //alert('Petición realizada');
+  		 }
+
+  });
+}
+
+function inicioEmpresa(via, datos){
+  $.ajax({
+         url:"/api/auth/authenticate/"+via,
+         type:"POST",
+         contentType:"application/json",
+         dataType:"json",
+
+         data:JSON.stringify(datos),
+
+         success: function(rta) {
+             localStorage.email = datos.email
+             Cookies.set('token',rta['token']);
+             window.location.replace("inicioAdmin.html");
+         },
+         error: function(xhr, status) {
+             alert('Usuario no existente');
+         },
+         complete: function(xhr, status) {
+             //alert('Petición realizada');
+         }
+
+    });
+}
+
+function inicioAdmin(via, datos){
+  $.ajax({
+
+  		 url:"/api/auth/authenticate/"+via,
+  		 type:"POST",
+  		 contentType:"application/json",
+  		 dataType:"json",
+
+  		 data:JSON.stringify(datos),
+
+  		 success: function(rta) {
+  		     localStorage.email = datos.email
+             Cookies.set('token',rta['token']);
+             window.location.replace("inicioAdmin.html");
+  		 },
+  		 error: function(xhr, status) {
+  			 alert('Usuario no existente');
+  		 },
+  		 complete: function(xhr, status) {
+  			 //alert('Petición realizada');
+  		 }
+
+  });
+}
+
