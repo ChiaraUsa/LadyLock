24eb5230eae8eb9060b2938aa2e0c6c35fb3d82d
sebas03999 <143473535+sebas03999@users.noreply.github.com>
2023-09-30 22:13:29 -0500

    Delete appAcoso directory

--- appAcoso/src/main/java/com/example/app/entidades/Role.java
+++
@@ -1,8 +0,0 @@
-package com.example.app.entidades;
-
-public enum Role {
-
-    USER,
-    EMPRESA,
-    ADMIN
-}
--- appAcoso/src/main/java/com/example/app/repository/LugaresCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.app.repository;
-
-import com.example.app.entidades.Empresa;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LugaresCrudRepository extends CrudRepository<Empresa,Integer> {
-}
