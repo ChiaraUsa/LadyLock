e5e213fa4349fb567a9392c35561c10f97b68d85
ChiaraUsa <chiara.valenzuela01@correo.usa.edu.co>
2023-10-13 14:24:34 -0500

    Preparación de espacio para consejos.

--- src/main/resources/static/js/usuario/consejos.js
+++ src/main/resources/static/js/usuario/consejos.js
@@ -1,9 +1,36 @@
 $(document).ready(function() {
-    setTimeout(function() {
-        $(".content").fadeOut(1500);
-    },3000);
- 
-    setTimeout(function() {
-        $(".content2").fadeIn(1500);
-    },6000);
+    function mostrarContenido(index) {
+        $(".content").eq(index).fadeIn(1500);
+        setTimeout(function() {
+            $(".content").eq(index).fadeOut(1500, function() {
+                if (index < 2) {
+                    mostrarContenido(index + 1);
+                } else {
+                    mostrarContenido(0);
+                }
+            });
+        }, 3000);
+    }
+    mostrarContenido(0);
 });
+
+var miElemento = document.getElementById('consejos'); // Reemplaza 'miElemento' con el ID de tu elemento
+
+// Obtén el objeto de medidas
+var medidas = miElemento.getBoundingClientRect();
+
+// Ahora puedes acceder a diferentes propiedades del objeto de medidas
+var ancho = medidas.width; // Ancho del elemento
+var alto = medidas.height; // Alto del elemento
+var top = medidas.top; // Distancia desde la parte superior del viewport
+var left = medidas.left; // Distancia desde la parte izquierda del viewport
+var right = medidas.right; // Distancia desde la parte derecha del viewport
+var bottom = medidas.bottom; // Distancia desde la parte inferior del viewport
+
+// Imprime las medidas en la consola
+console.log('Ancho: ' + ancho);
+console.log('Alto: ' + alto);
+console.log('Top: ' + top);
+console.log('Left: ' + left);
+console.log('Right: ' + right);
+console.log('Bottom: ' + bottom);
