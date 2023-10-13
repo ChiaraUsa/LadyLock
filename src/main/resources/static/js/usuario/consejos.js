469ed961f09e3bba1eb232caf3630040ce1b785b
ChiaraUsa <chiara.valenzuela01@correo.usa.edu.co>
2023-10-13 14:45:51 -0500

    Finalización de consejos al usar la aplicación.

--- src/main/resources/static/js/usuario/consejos.js
+++ src/main/resources/static/js/usuario/consejos.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         $(".content").eq(index).fadeIn(1500);
         setTimeout(function() {
             $(".content").eq(index).fadeOut(1500, function() {
-                if (index < 2) {
+                if (index < 3) {
                     mostrarContenido(index + 1);
                 } else {
                     mostrarContenido(0);
@@ -13,24 +13,3 @@ $(document).ready(function() {
     }
     mostrarContenido(0);
 });
-
-var miElemento = document.getElementById('consejos'); // Reemplaza 'miElemento' con el ID de tu elemento
-
-// Obtén el objeto de medidas
-var medidas = miElemento.getBoundingClientRect();
-
-// Ahora puedes acceder a diferentes propiedades del objeto de medidas
-var ancho = medidas.width; // Ancho del elemento
-var alto = medidas.height; // Alto del elemento
-var top = medidas.top; // Distancia desde la parte superior del viewport
-var left = medidas.left; // Distancia desde la parte izquierda del viewport
-var right = medidas.right; // Distancia desde la parte derecha del viewport
-var bottom = medidas.bottom; // Distancia desde la parte inferior del viewport
-
-// Imprime las medidas en la consola
-console.log('Ancho: ' + ancho);
-console.log('Alto: ' + alto);
-console.log('Top: ' + top);
-console.log('Left: ' + left);
-console.log('Right: ' + right);
-console.log('Bottom: ' + bottom);
