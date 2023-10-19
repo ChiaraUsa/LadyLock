a6b97223829febf2da9207721ae990e17fd197c0
sebas03999 <143473535+sebas03999@users.noreply.github.com>
2023-10-18 22:37:29 -0500

    Delete src directory

--- src/main/java/com/example/app/repository/UsuarioCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app.repository;
-
-import com.example.app.entidades.Usuario;
-import org.springframework.data.repository.CrudRepository;
-import java.util.Optional;
-
-public interface UsuarioCrudRepository extends CrudRepository<Usuario,Integer> {
-    Optional<Usuario> findByEmail (String email);
-}
