fb6b5d8783b662c39c834e6c2dd8010ee4356e4e
LuisAngel021 <110065864+LuisAngel021@users.noreply.github.com>
2023-10-29 22:48:55 -0500

    Add files via upload

--- appAcoso/target/classes/static/js/usuario/formulario.js
+++ appAcoso/target/classes/static/js/usuario/formulario.js
@@ -1,24 +1,13 @@
-var chatPage = document.querySelector('#chat-page');
-var formulario = document.querySelector('#cont-formulario')
+$(document).ready(function() {
+  enviarSolicitud();
+});
 
 function enviarSolicitud() {
-    chatPage.classList.remove('hidden');
-    formulario.classList.add('hidden');
-
-    let emergencia = {};
-    emergencia.userName = document.querySelector('#name').value
-    emergencia.userEmail = localStorage.email
-    emergencia.conductorName = document.querySelector("#nombre").value;
-    emergencia.marcaAuto = document.querySelector("#marca").value;
-    emergencia.modeloAuto = document.querySelector("#modelo").value;
-    emergencia.colorAuto = document.querySelector("#color").value;
-    emergencia.descripcion = document.querySelector("#descripcion").value;
 
     $.ajax({
-         url:"/api/admin/newEmergencia",
+         url:"/api/admin/newEmergencia?userEmail="+localStorage.email,
          type:"POST",
          contentType:"application/json",
-         data:JSON.stringify(emergencia),
          headers:{
             "Authorization": "Bearer "+ Cookies.get('token')
          },
