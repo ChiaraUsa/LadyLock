4b18a83bcdbf5e0d29b86516dac8b2933e4bd363
ChiaraUsa <chiara.valenzuela01@correo.usa.edu.co>
2023-10-31 19:02:31 -0500

    Eliminación de archivos

--- src/main/resources/static/js/usuario/map.js
+++ src/main/resources/static/js/usuario/map.js
@@ -4,9 +4,10 @@ var helpContainer2 = document.getElementById("help-container2");
 var helpContainer3 = document.getElementById("help-container3");
 var helpContainer4 = document.getElementById("help-container4");
 var volver = document.getElementById("Volver");
+var mapShown = false;
 
 var headers = {
-    Authorization: "Bearer "+ Cookies.get('token') // Reemplaza 'tuTokenJWT' con tu token real
+    Authorization: "Bearer "+ Cookies.get('token') 
 };
 var stompClient = null;
 var socket = new SockJS('/ws');
@@ -17,64 +18,95 @@ var socket = new SockJS('/ws');
             });
         });
 
-// Función para mostrar u ocultar el mapa
-function toggleMap() {
-    var mapContainer = document.getElementById('map');
+    // Función para mostrar u ocultar el mapa
+    function toggleMap() {
+        var mapContainer = document.getElementById('map');
 
-    if (mapContainer.style.display === "none" || mapContainer.style.display === "") {
-        // Mostrar mapa
-        mapContainer.style.display = "block";
-        mostrarMapa();
-        helpContainer4.style.width = '8%';
-        helpContainer4.style.marginTop = '10%';
-        helpContainer4.style.float = 'left';
-        volver.style.display ="block";
-    } else {
-        // Ocultar mapa
-        mapContainer.style.display = "none";
+        if (mapContainer.style.display === "none" || mapContainer.style.display === "") {
+            // Mostrar mapa
+            mapContainer.style.display = "block";
+            helpContainer4.style.width = '8%';
+            helpContainer4.style.marginTop = '10%';
+            helpContainer4.style.float = 'left';
+            volver.style.display ="block";
+            mostrarMapa();
+            // Enviar la ubicación del mapa en intervalos de 5 segundos
+            setInterval(sendLocation, 5000);
+        } else {
+            // Ocultar mapa
+            mapContainer.style.display = "none";
+        }
     }
-}
 
 // Función para mostrar el mapa
-function mostrarMapa() {
+    function mostrarMapa() {
+        // Crea un mapa en el div con id "map"
+        var map = L.map('map').setView([0, 0], 2); // Centrado en latitud 0, longitud 0, y zoom 2
 
-    // Crea un mapa en el div con id "map"
-    var map = L.map('map').setView([0, 0], 2); // Centrado en latitud 0, longitud 0, y zoom 2
+        // Agrega una capa de mapa base
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-    // Agrega una capa de mapa base
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        // Obtiene la ubicación del usuario y agrega un marcador en el mapa
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var lat = position.coords.latitude;
+                var lng = position.coords.longitude;
 
-    // Obtiene la ubicación del usuario y agrega un marcador en el mapa
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var lat = position.coords.latitude;
-            var lng = position.coords.longitude;
+                // Utiliza el servicio de geocodificación de Nominatim para obtener detalles del lugar
+                var apiUrl = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`;
 
-            // Utiliza el servicio de geocodificación de Nominatim para obtener detalles del lugar
-            var apiUrl = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`;
+                fetch(apiUrl)
+                    .then(response => response.json())
+                    .then(data => {
+                        var locationInfo = data.display_name; // Obtiene la información del lugar
+                        stompClient.send('/app/application', headers, JSON.stringify({'text': locationInfo }))
+                        var userMarker = L.marker([lat, lng]).addTo(map);
+                        userMarker.bindPopup(locationInfo).openPopup();
+                    }).catch(error => {
+                        console.error('Error en la geocodificación:', error);
+                    });
 
-            fetch(apiUrl)
-                .then(response => response.json())
-                .then(data => {
-                    var locationInfo = data.display_name; // Obtiene la información del lugar
-                    stompClient.send('/app/application', headers, JSON.stringify({'text': locationInfo }))
-                    var userMarker = L.marker([lat, lng]).addTo(map);
-                    userMarker.bindPopup(locationInfo).openPopup();
-                }).catch(error => {
-                    console.error('Error en la geocodificación:', error);
-                });
-
-            map.setView([lat, lng], 15); // Cambia la vista del mapa a la ubicación del usuario
-        }, function () {
-            // Manejo de errores
-            alert('No se pudo obtener la ubicación');
-        });
-    } else {
-        alert('La geolocalización no es compatible con este navegador.');
+                map.setView([lat, lng], 15); // Cambia la vista del mapa a la ubicación del usuario
+            }, function () {
+                // Manejo de errores
+                alert('No se pudo obtener la ubicación');
+            });
+        } else {
+            alert('La geolocalización no es compatible con este navegador.');
+        }
+        helpContainer.style.display = "none";
+        helpContainer2.style.display = "none";
+        helpContainer3.style.display = "none";
     }
-    helpContainer.style.display = "none";
-    helpContainer2.style.display = "none";
-    helpContainer3.style.display = "none";
+
+    // Función para enviar la ubicación al servidor
+    function sendLocation() {
+        // Obtener la ubicación del usuario y enviarla al servidor
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var lat = position.coords.latitude;
+                var lng = position.coords.longitude;
+                var apiUrl = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`;
+
+                fetch(apiUrl)
+                    .then(response => response.json())
+                    .then(data => {
+                        var locationInfo = data.display_name; // Obtiene la información del lugar
+                        stompClient.send('/app/application', headers, JSON.stringify({'text': locationInfo }))
+                        var userMarker = L.marker([lat, lng]).addTo(map);
+                        userMarker.bindPopup(locationInfo).openPopup();
+                    }).catch(error => {
+                        console.error('Error en la geocodificación:', error);
+                    });
+            }, function () {
+                // Manejo de errores
+                console.log('No se pudo obtener la ubicación');
+            });
+        } else {
+            console.log('La geolocalización no es compatible con este navegador.');
+        }
     }
+  // Escucha el clic del botón para mostrar el mapa
+mostrarMapaButton.addEventListener('click', toggleMap);
