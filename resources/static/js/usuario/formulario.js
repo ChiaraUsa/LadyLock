2deefb50443a2684200e4e62a11eb0bdde7297a7
sebas03999 <143473535+sebas03999@users.noreply.github.com>
2023-11-01 20:18:35 -0500

    Comunicacion mas estable usuario-admin

--- resources/static/js/usuario/formulario.js
+++ resources/static/js/usuario/formulario.js
@@ -25,3 +25,44 @@ function enviarSolicitud() {
     });
 
 }
+
+//por si cierra la ventana
+window.addEventListener('beforeunload', function (e) {
+    $.ajax({
+         url:"/api/admin/cierraUsuario?idAdmin="+localStorage.idAdmin,
+         type:"POST",
+         contentType:"application/json",
+         success: function(rta) {
+             localStorage.idAdmin = -1;
+             window.location.replace("CentroAyuda.html");
+         },
+         error: function(xhr, status) {
+             alert('Admin no existente');
+         },
+         complete: function(xhr, status) {
+             //alert('Petición realizada');
+         }
+    });
+});
+
+//por si cambia la ventana
+window.addEventListener('popstate', function(event) {
+
+  alert("Se cerrara el chat");
+
+  $.ajax({
+       url:"/api/admin/cierraUsuario?idAdmin="+localStorage.idAdmin,
+       type:"POST",
+       contentType:"application/json",
+       success: function(rta) {
+           localStorage.idAdmin = -1;
+           window.location.replace("CentroAyuda.html");
+       },
+       error: function(xhr, status) {
+           alert('Admin no existente');
+       },
+       complete: function(xhr, status) {
+           //alert('Petición realizada');
+       }
+    });
+});
